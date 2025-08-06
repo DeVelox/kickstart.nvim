810ab35adb52213a213a9faf815c0fc52f53aa1b
DeVelox <DeVelox@users.noreply.github.com>
2025-08-06 21:14:29 +0200

    switch to lm-studio

--- init.lua
+++ init.lua
@@ -808,9 +808,9 @@ require('lazy').setup({
       provider_options = {
         openai_fim_compatible = {
           api_key = 'TERM',
-          name = 'Ollama',
-          end_point = 'http://localhost:11434/v1/completions',
-          model = 'qwen2.5-coder:3b-base-q8_0',
+          name = 'LM Studio',
+          end_point = 'http://localhost:1234/v1/completions',
+          model = 'PLACEHOLDER',
           optional = {
             max_tokens = 56,
             top_p = 0.9,
